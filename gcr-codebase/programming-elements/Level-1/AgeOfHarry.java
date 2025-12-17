270c161d41639cf55dc12c5ca9d05ea97b5789a3
vedansh90 <vedanshbarve51@gmail.com>
2025-12-17 11:56:02 +0530

    [Vedansh] : Added Level-1 AgeOfHarry file

---
+++ gcr-codebase/programming-elements/Level-1/AgeOfHarry.java
@@ -0,0 +1,9 @@
+class AgeOfHarry
+{
+    public static void main(String args[])
+    {
+    	int birthYear = 2000;
+	int currentYear = 2024;
+	System.out.print(" Harry's age in 2024 is: " + (currentYear-birthYear));
+    }
+}
