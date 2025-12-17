40654a949bf3bcc096772c463ff929a67f3b602a
vedansh90 <vedanshbarve51@gmail.com>
2025-12-17 16:26:49 +0530

    [Vedansh] : Added Level-2 calcTotalPrice file

---
+++ gcr-codebase/programming-elements/Level-2/calcTotalPrice.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class calcTotalPrice
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);	
+	int unitPrice;
+	int quantity;
+	System.out.print("Enter unit price: ");
+	unitPrice = input.nextInt();
+	System.out.print("Enter quantity: ");
+	quantity = input.nextInt();
+	int totalPrice = unitPrice*quantity;
+	
+	System.out.print("The total purchase price is INR" + totalPrice + " if the quantity "+ quantity +"and unit price is INR" + unitPrice);
+
+    }
+}
