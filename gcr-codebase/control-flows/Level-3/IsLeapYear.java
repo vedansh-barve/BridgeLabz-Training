ea65a886c299d9ace4bfea7b28372b17639520c2
vedansh90 <vedanshbarve51@gmail.com>
2025-12-18 16:28:52 +0530

    [Vedansh] : Added control-flows Level-3 IsLeapYear file

---
+++ gcr-codebase/control-flows/Level-3/IsLeapYear.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class IsLeapYear
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+	int year;
+	System.out.print("Enter Year: ");
+	year = input.nextInt();
+	
+	if(year >= 1582 && year <= 2025)
+	{	
+	    if(year%400 == 0 || (year % 4 == 0 && year % 100 != 0))
+	    	System.out.println("Leap Year");
+	    else
+	    	System.out.println("Not a Leap Year");
+
+	}
+	else
+	{
+	System.out.println("Not a valid year for leap year conditions");
+	}
+    }
+}
+
