d7a0e2cf26d69b99d447a6f04ba4fcea37037f32
vedansh90 <vedanshbarve51@gmail.com>
2025-12-19 16:39:14 +0530

    [Vedansh] : Added Array Level-2 StoreDigitsInArray file

--- gcr-codebase/arrays/Level-2/StoreDigitsInArray.java
+++ gcr-codebase/arrays/Level-2/StoreDigitsInArray.java
@@ -5,44 +5,41 @@ public class StoreDigitsInArray
     public static void main(String args[])
     {
         Scanner input = new Scanner(System.in);
-	int number;
+	long number;
 	System.out.print("Enter Number: ");
-	number = input.nextInt();
-	int originalNumber = number;
-	int totalDigits = 0;
+	number = input.nextLong();
+	long originalNumber = number;
+	int totalDigits = 10;
 	int largest = -1;
+
 	while(number != 0)
 	{
-	    int digit = number%10;
-	    totalDigits++;
+	    int digit = (int)number%10;
 	    number /= 10;
 	    if(digit > largest)
 		largest = digit;
 	}
 	
-	int[] digitsArr = new int[totalDigits];
+	int[] digitsArr = new int[10];
 	number = originalNumber;
 	int index = 0;
-	
 	int secondLargest = -1;
-	while(number != 0)
+
+	while (number != 0)
 	{
-	    int digit = number%10;
-	    digitsArr[index++] = digit;
-	    number /= 10;
-	    if(digit > secondLargest && digit != largest)
-		secondLargest = digit;
+    	if (index == totalDigits)
+        	break;   
+
+    	int digit = (int)number % 10;
+   	digitsArr[index++] = digit;
+    	number /= 10;
+
+    	if (digit > secondLargest && digit != largest)
+       		secondLargest = digit;
 	}
+
 	
-	int i = 0, j = totalDigits - 1;
-	while(i < j)
-	{
-	    int temp = digitsArr[i];
-	    digitsArr[i] = digitsArr[j];
-	    digitsArr[j] = temp;
-	    i++;
-	    j--;
-	}	
+		
 	System.out.println("The largest Element is: " + largest);
 	System.out.println("The second largest Element is: " + secondLargest);
 
