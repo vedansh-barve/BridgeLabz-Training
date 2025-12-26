c9cf04e617da8f32764414538f2a26c07115c4e7
vedansh90 <vedanshbarve51@gmail.com>
2025-12-26 14:19:04 +0530

    [Vedansh] : Added Leetcode Arrays Day-1 RemoveDuplicates file

---
+++ leet-code-codebase/arrays/day-1/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int count = 1; 
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
