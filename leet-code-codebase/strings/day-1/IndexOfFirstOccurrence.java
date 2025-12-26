4dd103ec010200605bc255d9af896818afce96e8
vedansh90 <vedanshbarve51@gmail.com>
2025-12-26 22:59:00 +0530

    [Vedansh] : Added Leet-code Strings day-1 IndexOfFirstOccurrence

---
+++ leet-code-codebase/strings/day-1/IndexOfFirstOccurrence.java
@@ -0,0 +1,21 @@
+class IndexOfFirstOccurrence {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == m) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
