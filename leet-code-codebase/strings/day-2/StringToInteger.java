07c44ea31cd203ec57ebe9036b06619d9b40c9ee
vedansh90 <vedanshbarve51@gmail.com>
2025-12-28 22:26:57 +0530

    [Vedansh] : Added Leetcode Strings day-2 StringToInteger file

---
+++ leet-code-codebase/strings/day-2/StringToInteger.java
@@ -0,0 +1,38 @@
+class StringToInteger {
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        long result = 0;
+
+        // 1. Ignore leading whitespaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        // 3. Convert digits
+        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            result = result * 10 + (s.charAt(i) - '0');
+
+            // 4. Handle overflow
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int) (sign * result);
+    }
+}
