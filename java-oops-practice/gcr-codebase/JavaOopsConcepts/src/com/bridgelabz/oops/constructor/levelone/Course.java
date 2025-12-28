8dfaa72e40953c0bc45d17622fef83b7ae9f28c5
vedansh90 <vedanshbarve51@gmail.com>
2025-12-28 21:20:10 +0530

    [Vedansh] : Added Constructor Course file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/constructor/levelone/Course.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.oops.constructor.levelone;
+
+class Course {
+
+    // Instance Variables
+    String courseName;
+    int duration;  
+    double fee;
+
+    // Class Variable 
+    static String instituteName = "Tech Institute";
+
+    // Constructor
+    Course(String courseName, int duration, double fee) {
+        this.courseName = courseName;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    // Instance Method
+    void displayCourseDetails() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " months");
+        System.out.println("Fee: ₹" + fee);
+        System.out.println("Institute: " + instituteName);
+        System.out.println("------------------------");
+    }
+
+    // Class Method
+    static void updateInstituteName(String newInstituteName) {
+        instituteName = newInstituteName;
+    }
+    
+    public static void main(String[] args) {
+
+        Course c1 = new Course("Java Full Stack", 6, 45000);
+        Course c2 = new Course("Data Science", 8, 60000);
+
+        c1.displayCourseDetails();
+        c2.displayCourseDetails();
+
+        // Update institute name for all courses
+        Course.updateInstituteName("Code Academy");
+
+        c1.displayCourseDetails();
+        c2.displayCourseDetails();
+    }
+}
