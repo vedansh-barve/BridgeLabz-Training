0d3d12fff1ce8d66ebdc51b80c2585633bc6e1ef
vedansh90 <vedanshbarve51@gmail.com>
2026-01-03 11:59:15 +0530

    [Vedansh] : Added OOPS Encapsulation RideHailingDemo file

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/ridehailing/RideHailingDemo.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/encapsulation/ridehailing/RideHailingDemo.java
@@ -4,14 +4,14 @@ public class RideHailingDemo {
 	public static void printFareEstimate(Vehicle v, double distance) {
         System.out.println(v.getVehicleDetails());
         System.out.println("Driver " + v.getDriverName() + " is currently at: " + v.getCurrentLocation());
-        System.out.println("Estimated Fare for " + distance + "km: $" + v.calculateFare(distance));
+        System.out.println("Estimated Fare for " + distance + "km: " + v.calculateFare(distance));
         System.out.println("------------------------------------\n");
     }
 	
 	 public static void main(String[] args) {
-	        Vehicle myRide = new Car("CAR-77", "Alex");
-	        Vehicle ecoRide = new Bike("BKE-12", "Sam");
-	        Vehicle budgetRide = new Auto("AUTO-09", "Raj");
+	        Vehicle myRide = new Car("CAR-77", "Ravi");
+	        Vehicle ecoRide = new Bike("BKE-12", "Swaraj");
+	        Vehicle budgetRide = new Auto("AUTO-09", "Ashish");
 
 	        myRide.updateLocation("Downtown");
 	        ecoRide.updateLocation("Central Station");
