a80930b95fcf6a6c3b39b628f114d4a71071c167
vedansh90 <vedanshbarve51@gmail.com>
2026-01-15 14:40:51 +0530

    [Vedansh] : Added Scenario_BasedDSA SmartShelfDemo file

---
+++ dsa-practice/scenario-based-codebase/scenariobasedproblems/src/com/day4/smartshelf/Book.java
@@ -0,0 +1,9 @@
+package com.day4.smartshelf;
+
+public class Book {
+	
+	public String title;
+	public Book(String title) {
+		this.title = title;
+	}
+}
---
+++ dsa-practice/scenario-based-codebase/scenariobasedproblems/src/com/day4/smartshelf/SmartShelfDemo.java
@@ -0,0 +1,32 @@
+package com.day4.smartshelf;
+
+public class SmartShelfDemo {
+
+	public static void insertionSort(Book books[]) {
+		int n = books.length;
+		
+		for(int i=1; i<n; i++) {
+			Book key = books[i];
+			int j = i - 1;
+			
+			while(j >= 0 && books[j].title.compareToIgnoreCase(key.title) > 0) {
+				books[j + 1] = books[j];
+				j--;
+			}
+			books[j + 1] = key;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Book[] books = {new Book("Data Structures"), new Book("Algorithms"), new Book("Computer Networks"), new Book("Operating Systems")};
+
+		 insertionSort(books);
+
+		 System.out.println("📚 Sorted Reading List:");
+		 for (Book b : books) {
+			 System.out.println(b.title);
+		 }
+	}
+
+}
