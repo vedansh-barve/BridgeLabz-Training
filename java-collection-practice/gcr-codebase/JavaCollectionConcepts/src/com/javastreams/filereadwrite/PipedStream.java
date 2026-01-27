b33491139b7113d1bcddcb7c4ec74c7d6147beed
vedansh90 <vedanshbarve51@gmail.com>
2026-01-27 12:01:54 +0530

    [Vedansh] : Added Streams PipedStream file

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/javastreams/filereadwrite/PipedStream.java
@@ -0,0 +1,68 @@
+package com.javastreams.filereadwrite;
+
+import java.io.*;
+
+class WriterThread extends Thread {
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = "Hello from Writer Thread!";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.out.println("Error in Writer Thread.");
+            e.printStackTrace();
+        }
+    }
+}
+
+class ReaderThread extends Thread {
+    private PipedInputStream pis;
+
+    public ReaderThread(PipedInputStream pis) {
+        this.pis = pis;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int data;
+            while ((data = pis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            pis.close();
+        } catch (IOException e) {
+            System.out.println("Error in Reader Thread.");
+            e.printStackTrace();
+        }
+    }
+}
+
+public class PipedStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            reader.start();
+            writer.start();
+
+        } catch (IOException e) {
+            System.out.println("Error connecting piped streams.");
+            e.printStackTrace();
+        }
+	}
+
+}
