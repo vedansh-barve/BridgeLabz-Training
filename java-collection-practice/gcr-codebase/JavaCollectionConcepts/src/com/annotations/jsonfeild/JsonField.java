da9f71e89a187d43016be4e436ab2c4792b3920d
vedansh90 <vedanshbarve51@gmail.com>
2026-01-27 15:11:17 +0530

    [Vedansh] : Added Annotations JSONFeild file

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/annotations/jsonfeild/JsonField.java
@@ -0,0 +1,13 @@
+package com.annotations.jsonfeild;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+// Field-level annotation for JSON mapping
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface JsonField {
+	String name();
+}
---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/annotations/jsonfeild/JsonSerializer.java
@@ -0,0 +1,42 @@
+package com.annotations.jsonfeild;
+import java.lang.reflect.Field;
+
+public class JsonSerializer {
+	public static String toJson(Object obj) {
+		StringBuilder json = new StringBuilder();
+		json.append("{");
+
+		Field[] fields = obj.getClass().getDeclaredFields();
+
+		for (int i = 0; i < fields.length; i++) {
+		    Field field = fields[i];
+		    field.setAccessible(true);
+
+		    if (field.isAnnotationPresent(JsonField.class)) {
+			JsonField annotation = field.getAnnotation(JsonField.class);
+
+			try {
+			    json.append("\"").append(annotation.name()).append("\":");
+
+			    Object value = field.get(obj);
+
+			    if (value instanceof String) {
+				json.append("\"").append(value).append("\"");
+			    } else {
+				json.append(value);
+			    }
+
+			    if (i < fields.length - 1) {
+				json.append(",");
+			    }
+
+			} catch (IllegalAccessException e) {
+			    System.out.println("Serialization error.");
+			}
+		    }
+		}
+
+		json.append("}");
+		return json.toString();
+	    }
+}
