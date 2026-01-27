3b5932cc54e7538acdeda242d8719102e43804c4
vedansh90 <vedanshbarve51@gmail.com>
2026-01-27 18:47:02 +0530

    [Vedansh] : Added Reflection PrivateMethod file

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/privatemethod/InvokePrivateMethod.java
@@ -0,0 +1,22 @@
+package com.reflection.privatemethod;
+import java.lang.reflect.Method;
+
+public class InvokePrivateMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		    Calculator calc = new Calculator();
+		    Method method = Calculator.class.getDeclaredMethod("multiply", int.class, int.class);
+
+		    method.setAccessible(true);
+		    int result = (int) method.invoke(calc, 4, 5);
+
+		    System.out.println("Result: " + result);
+
+		} catch (Exception e) {
+		    System.out.println("Invocation failed.");
+		}
+	}
+
+}
